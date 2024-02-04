03ef41ee623bc4b68cc1b3171bfeb99727b12654
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-02-04 13:38:04 +0545

    New Features Enhancement
    Some new Changes we made in the ui Like navmenu protection

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import Contact from "./pages/Contact";
 import Policy from "./pages/Policy";
 import Pagenotfound from "./pages/Pagenotfound";
 import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
-        <Route path="*" element={<Pagenotfound />} />
+        <Route path="*" element={<Pagenotfound />} /> 
       </Routes>
     </>
   );
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { NavLink,Link } from "react-router-dom";
 import { TiShoppingCart } from "react-icons/ti";
+import { useAuth } from "../../context/auth";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const [auth,setAuth] = useAuth()
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user: null,
+      token: "",
+    });
+    localStorage.removeItem("auth");
+    toast.success("Logout Successfully");
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -38,7 +50,9 @@ const Header = () => {
                   Category
                 </NavLink>
               </li>
-              <li className="nav-item">
+              {
+                !auth.user ? (<>
+                 <li className="nav-item">
                 <NavLink to="/register" className="nav-link">
                   Register
                 </NavLink>
@@ -48,6 +62,14 @@ const Header = () => {
                   Login
                 </NavLink>
               </li>
+                </>) : (<>
+                  <li className="nav-item">
+                <NavLink onClick={handleLogout} to="/login" className="nav-link">
+                  Logout
+                </NavLink>
+              </li>
+                </>)
+              }
               <li className="nav-item">
                 <NavLink to="/cart" className="nav-link">
                   Cart (0)
