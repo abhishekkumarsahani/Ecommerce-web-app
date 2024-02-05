f94cb5b66c61e54f75f45df05033fac4c9454f17
abhishekkumarsahani <126187842+abhishekkumarsahani@users.noreply.github.com>
2024-02-06 00:32:31 +0545

    Enhance Backend of Product Page

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,46 @@
-import express from 'express'
-import { createProductController } from '../controllers/productController.js';
-import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
-import formidable from 'express-formidable'
+import express from "express";
+import {
+  createProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  deleteProductController,
+  updateProductController
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
 
-const router = express.Router()
+const router = express.Router();
 
-//routes 
+//routes
 //routes
 router.post(
-    "/create-product",
-    requireSignIn,
-    isAdmin,
-    formidable(),
-    createProductController
-  );
-  export default router;
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+
+
+export default router;
